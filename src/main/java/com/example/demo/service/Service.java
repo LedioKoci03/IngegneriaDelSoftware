c14ad5ddd6bed5cf914cd901662326d861be5f5a
LedioKoci03 <127228600+LedioKoci03@users.noreply.github.com>
2025-01-13 22:06:37 +0100

    classe service modificata

--- src/main/java/com/example/demo/service/Service.java
+++ src/main/java/com/example/demo/service/Service.java
@@ -1,5 +1,14 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Repository;
+
 @org.springframework.stereotype.Service
 public class Service {
+
+    Repository repository;
+
+    public Service(Repository repository){
+        this.repository = repository;
+    }
+
 }
